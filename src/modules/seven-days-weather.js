881d04cd8fb0d1770cd833d86b3a18f765c0e42d
ikari-shirei <algrsyu@protonmail.com>
2021-07-14 22:29:39 +0300

    Create forecast elements

---
+++ src/modules/seven-days-weather.js
@@ -0,0 +1,26 @@
+/* eslint-disable space-before-function-paren */
+function sevenDaysWeather(dayName, icon, temp) {
+  const sevenDaysContainer = document.querySelector('#sevenDaysContainer');
+
+  const sevenDays = document.createElement('div');
+  sevenDays.classList.add('seven-days');
+
+  const weekDay = document.createElement('div');
+  weekDay.classList.add('week-day');
+  weekDay.textContent = dayName;
+  sevenDays.appendChild(weekDay);
+
+  const sevenDaysIcon = document.createElement('img');
+  sevenDaysIcon.classList.add('seven-days-icon');
+  sevenDaysIcon.src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+  sevenDays.appendChild(sevenDaysIcon);
+
+  const sevenDaysTemp = document.createElement('div');
+  sevenDaysTemp.classList.add('seven-days-temp');
+  sevenDaysTemp.textContent = temp;
+  sevenDays.appendChild(sevenDaysTemp);
+
+  sevenDaysContainer.appendChild(sevenDays);
+}
+
+export default sevenDaysWeather;
